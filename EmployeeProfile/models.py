faf4fe73674864a571dcfb38d03eff3c1e7301f5
hadoleamit <hadoleamit@gmail.com>
2019-07-10 12:09:26 +0530

    postgress sql

--- EmployeeProfile/models.py
+++ EmployeeProfile/models.py
@@ -4,7 +4,7 @@ class EmployeeDetails(models.Model):
     name  = models.CharField(max_length=100) 
     photo =models.ImageField(upload_to='Photos/')
     email = models.EmailField(max_length=30)
-    mobile = models.PositiveIntegerField(max_length=13)
+    mobile = models.CharField(max_length=13)
     designation = models.CharField(max_length=50)
     skills = models.TextField(max_length=500)
     about = models.TextField(max_length=500)
